eb431d8923dcd800985a17a4daf0bec28fcc3d80
Ye Thu Aung <yethuaung481995@gmail.com>
2025-07-02 16:43:15 +0630

    [Add] add tailwindcss and daisy ui

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/register", function (req, res, next) {
+  res.render("register");
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/tips", function (req, res, next) {
+  res.render("tips", { title: "Sustainable Tips" });
+});
+
+router.get("/challenges", function (req, res, next) {
+  res.render("challenges", { title: "Eco Challenges" });
+});
+
+router.get("/calculator", function (req, res, next) {
+  res.render("calculator", { title: "Eco Calculator" });
+});
+
+router.get("/blogs", function (req, res, next) {
+  res.render("blogs", { title: "Blog" });
+});
+
+router.get("/community", function (req, res, next) {
+  res.render("community", { title: "Community" });
+});
+
+router.get("/aboutus", function (req, res, next) {
+  res.render("aboutus", { title: "About Us" });
 });
 
 module.exports = router;
